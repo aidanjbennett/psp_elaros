a33fc0866c4d7cfe500ac284dafa7840243b095d
Aidan <57372629+aidanjbennett@users.noreply.github.com>
2026-02-17 15:52:43 +0000

    Play with notifications (#8)
    * Install Flutter Local Notifications
    
    * Setup permissions for Android
    
    * Setup permissions for iOS
    
    * Add permissions for IOS
    
    * Install Timezone
    
    * Add Notification Service
    
    * Add button to send test notification
    
    * Updated Android packages
    
    * Updated Android packages
    
    * Fixed notification icon breaking app launch on Android platform
    
    ---------
    
    Co-authored-by: Asher Denno <brokenboatman@gmail.com>

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
@@ -10,30 +9,34 @@ android {
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
+    // ONLY ONE defaultConfig block
+    defaultConfig {
+        applicationId = "com.example.psp_elaros"
+        minSdk = 26
+        targetSdk = 34
+
+        // These MUST be assigned correctly to resolve the Manifest error
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+
+        multiDexEnabled = true
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
+        isCoreLibraryDesugaringEnabled = true
     }
 
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
-    }
-
-    defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.psp_elaros"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 26
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
+    // Modern way to set Java 17 for Kotlin (Fixes the deprecation warning)
+    kotlin {
+        compilerOptions {
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+        }
     }
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
@@ -41,4 +44,8 @@ android {
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
