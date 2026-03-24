93894c5012d5663a5108e125be646197254caeb0
Aidan Bennett <57372629+aidanjbennett@users.noreply.github.com>
2026-03-24 09:35:27 +0000

    Move heart metrics model into its own file

--- lib/data/repositories/health_repository.dart
+++ lib/data/repositories/health_repository.dart
@@ -2,23 +2,10 @@ import 'dart:io';
 
 import 'package:flutter/foundation.dart';
 import 'package:health/health.dart';
+import 'package:psp_elaros/data/models/heart_metrics_model.dart';
 import 'package:psp_elaros/data/models/heart_rate_data_model.dart';
 import 'package:psp_elaros/data/models/heart_rate_variability_rate_model.dart';
 import 'package:psp_elaros/data/models/sleep_model.dart';
-
-class HeartMetrics {
-  final List<HeartRate> heartRates;
-  final List<HeartRateVariabilityRate> hrvRates;
-  final double? averageHeartRate;
-  final double? averageHrv;
-
-  HeartMetrics({
-    required this.heartRates,
-    required this.hrvRates,
-    required this.averageHeartRate,
-    required this.averageHrv,
-  });
-}
 
 class HealthRepository {
   final Health _health = Health();
