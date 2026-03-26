da35e9f7135cc96322e56072c26fbeaca869b148
jhallam00 <158037841+jhallam00@users.noreply.github.com>
2026-03-26 10:29:03 +0000

    Add metrics screen

--- lib/screens/metrics_screen.dart
+++ lib/screens/metrics_screen.dart
@@ -5,11 +5,14 @@ import 'package:psp_elaros/style/app_style.dart';
 class MetricsScreen extends StatelessWidget {
   const MetricsScreen({super.key});
 
+  //current code is messy, after meeting refine it to widgets
+  //create if statements for on track, displaying HRV, etc
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: const Text("Bettertrack"),
+          title: const Text("Your Dashboard"),
           actions: [
             IconButton(
               icon: const Icon(Icons.settings),
@@ -26,110 +29,172 @@ class MetricsScreen extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Text(
-                "Your Metrics",
+                "Status Overview",
                 style: Theme.of(context).textTheme.titleLarge,
               ),
               const SizedBox(height: 16),
-              // Top row
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Expanded(
-                    child: Container(
-                      height: 210,
-                      decoration: BoxDecoration(
-                        color: AppColors.surface,
-                        borderRadius: BorderRadius.circular(16.0),
-                      ),
-                      child: const Align(
-                        child: Text(
-                          'Heart Rate Zones',
-                          textDirection: TextDirection.ltr,
-                          style: TextStyle(fontSize: 20, color: Colors.black87),
-                        ),
-                      ),
-                    ),
-                  ),
-
-                  SizedBox(width: 10),
-
-                  Expanded(
-                    child: Column(
+              Container(
+                padding: const EdgeInsets.all(16),
+                decoration: BoxDecoration(
+                  color: AppColors.surface,
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
                       children: [
-                        Container(
-                          height: 100,
-                          decoration: BoxDecoration(
-                            color: AppColors.surface,
-                            borderRadius: BorderRadius.circular(16.0),
-                          ),
-                          child: const Align(
-                            child: Text(
-                              'Steps',
-                              textDirection: TextDirection.ltr,
-                              style: TextStyle(fontSize: 20, color: Colors.black87),
-                            ),
-                          ),
+
+                        const Text(
+                          "Heart Rate",
+                          style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600, color: Colors.black87),
                         ),
 
-                        SizedBox(height: 10),
+                        const Spacer(),
 
-                        Container(
-                          height: 100,
-                          decoration: BoxDecoration(
-                            color: AppColors.surface,
-                            borderRadius: BorderRadius.circular(16.0),
-                          ),
-                          child: const Align(
-                            child: Text(
-                              'Sleep',
-                              textDirection: TextDirection.ltr,
-                              style: TextStyle(fontSize: 20, color: Colors.black87),
-                            ),
+                        const Text(
+                          "Status",
+                          style: TextStyle(fontSize: 18),
+                        ),
+
+                        const SizedBox(width: 10),
+
+                      ],
+                    ),
+
+                    const SizedBox(height: 16),
+
+                    Row(
+                      children: const [
+                        Text(
+                          "HRV:",
+                          style: TextStyle(fontSize: 26, fontWeight: FontWeight.w600, color: Colors.black87),
+                        ),
+                        SizedBox(width: 5),
+                        Text(
+                          "000",
+                          style: TextStyle(fontSize: 26, fontWeight: FontWeight.w600, color: Colors.black87),
+                        ),
+                        SizedBox(width: 10,),
+                        Text(
+                          'text demo',
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.red,
                           ),
                         ),
                       ],
                     ),
-                  ),
-                ],
+
+                    const SizedBox(height: 12),
+                    const Text(
+                      "Update on the user here",
+                      style: TextStyle(fontSize: 16, color: Colors.black87),
+                    ),
+                  ],
+                ),
               ),
 
               SizedBox(height: 10),
 
               Container(
-                height: 150,
-                width: 400,
+                padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 12),
                 decoration: BoxDecoration(
                   color: AppColors.surface,
                   borderRadius: BorderRadius.circular(16.0),
                 ),
-                child: const Align(
-                  child: Text(
-                    'Heart Rate Variability',
-                    textDirection: TextDirection.ltr,
-                    style: TextStyle(fontSize: 20, color: Colors.black87),
-                  ),
+                child: Column(
+                  children: [
+                    //sleep section
+                    Row(
+                      children: const [
+                        CircleAvatar(
+                          radius: 24,
+                          backgroundColor: Colors.purple,
+                          child: const Icon(Icons.bed, color: Colors.white),
+                        ),
+                        SizedBox(width: 12),
+
+                        Text(
+                          "Sleep",
+                          style: TextStyle(fontSize: 24, fontWeight: FontWeight.w600, color: Colors.black87),
+                        ),
+                        Spacer(),
+                        Text(
+                          'All good',
+                          style: TextStyle(fontSize: 24, fontWeight: FontWeight.w600, color: Colors.black87),
+                        ),
+                        SizedBox(width: 12),
+                        const CircleAvatar(
+                          radius: 18,
+                          backgroundColor: Colors.green,
+                          child: Icon(Icons.check, color: Colors.white, size: 24),
+                        ),
+                        const Icon(Icons.chevron_right),
+                      ],
+                    ),
+                    const Divider(thickness: 2,),
+
+                    //goals section
+                    const SizedBox(height: 10),
+                    Row(
+                      children: const [
+                        Icon(Icons.location_searching, color: Colors.white),
+                        SizedBox(width: 12),
+                        Text(
+                          "Goals",
+                          style: TextStyle(fontSize: 24, fontWeight: FontWeight.w600, color: Colors.black87),
+                        ),
+                        Spacer(),
+                        Text(
+                          'On track',
+                          style: TextStyle(fontSize: 24, fontWeight: FontWeight.w600, color: Colors.black87),
+                        ),
+                        SizedBox(width: 12),
+                        const CircleAvatar(
+                          radius: 18,
+                          backgroundColor: Colors.green,
+                          child: Icon(Icons.check, color: Colors.white, size: 24),
+                        ),
+                        const Icon(Icons.chevron_right),
+                      ],
+                    ),
+                    const Divider(thickness: 2,),
+                    const SizedBox(height: 10),
+
+                    //steps section
+                    Row(
+                      children: const [
+                        Icon(Icons.directions_walk, color: Colors.white,),
+                        SizedBox(width: 12),
+                        Text(
+                          "Steps",
+                          style: TextStyle(fontSize: 24, fontWeight: FontWeight.w600, color: Colors.black87),
+                        ),
+                        Spacer(),
+                        Text(
+                          'On track',
+                          style: TextStyle(fontSize: 24, fontWeight: FontWeight.w600, color: Colors.black87),
+                        ),
+                        SizedBox(width: 12),
+                        const CircleAvatar(
+                          radius: 18,
+                          backgroundColor: Colors.green,
+                          child: Icon(Icons.check, color: Colors.white, size: 24),
+                        ),
+                        const Icon(Icons.chevron_right),
+                      ],
+                    ),
+                  ],
+
                 ),
+
               ),
               SizedBox(height: 10),
-
-              Container(
-                height: 150,
-                width: 400,
-                decoration: BoxDecoration(
-                  color: AppColors.surface,
-                  borderRadius: BorderRadius.circular(16.0),
-                ),
-                child: const Align(
-                  child: Text(
-                    'Heart Rate',
-                    textDirection: TextDirection.ltr,
-                    style: TextStyle(fontSize: 20, color: Colors.black87),
-                  ),
-                ),
-              ),
             ],
           ),
         )
+
 
 
 
